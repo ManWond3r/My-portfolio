3ffb2ec1b837d89456d8c0198b91da12d673ea0a
HP <emanywanda41@gmail.com>
2025-06-28 16:55:12 +0300

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+
+toggleBtn.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    toggleBtn.textContent = body.classList.contains('dark')
+    ?'🌞'
+    :'🌙';
+
+})
+
+   
