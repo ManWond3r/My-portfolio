ad09571c636fefda0bf06f201cd070648f0b5812
HP <emanywanda41@gmail.com>
2025-06-30 01:36:13 +0300

    Updated project

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const toggleBtn = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
 const body = document.body;
 
 toggleBtn.addEventListener('click', () => {
     body.classList.toggle('dark');
-    toggleBtn.textContent = body.classList.contains('dark')
-    ?'🌞'
-    :'🌙';
+
+    themeIcon.src = body.classList.contains('dark')
+     ? "images/sun.png"
+     : "images/night.png";
 
 })
+
+const contactToggle = document.getElementById('contact-toggle');
+const contactOptions = document.getElementById('contact-options');
+
+contactToggle.addEventListener('click', () => {
+  contactOptions.style.display =
+    contactOptions.style.display === 'flex' ? 'none' : 'flex';
+});
+
 
    
